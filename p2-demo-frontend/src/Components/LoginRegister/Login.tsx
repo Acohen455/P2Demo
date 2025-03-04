53f658b04726e223c91d0ebde109870b7221e406
Acohen455 <aaroncohen455@gmail.com>
2025-03-04 09:06:17 -0700

    asdf

--- p2-demo-frontend/src/Components/LoginRegister/Login.tsx
+++ p2-demo-frontend/src/Components/LoginRegister/Login.tsx
@@ -51,7 +51,15 @@ export const Login:React.FC = () => {
 
         try{
 
-            const response = await axios.post("http://3.133.155.102:8080/auth/login", loginCreds, {withCredentials:true})
+            //EC2 AXIOS REQUEST
+            //NO LONGER EXISTS
+            //const response = await axios.post("http://3.133.155.102:8080/auth/login", loginCreds, {withCredentials:true})
+
+            //LOCAL AXIOS REQUEST
+            const response = await axios.post("http://localhost:8081/auth/login",
+                loginCreds, {withCredentials:true})
+
+
             //withCredentials lets us interact with sessions on the backend
             //every request that depends on the user being logged in, being an admin, etc, needs this
 
